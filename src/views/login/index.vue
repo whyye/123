d80eefd723f8f3579936b5fcdc1246c2cd044a14
whyye <whyye@126.com>
2021-07-06 13:31:24 +0800

    创建项目

---
+++ src/views/login/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="login">
+    login.....
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+ 
+export default {
+   name:"login",
+   data() {
+     return {
+       login:'wwwwww'
+     }
+   },
+}
+</script>
+
+<style lang="scss" scoped>
+  #login {
+    background-color: #333366;
+    height: 100vh;
+  }
+</style>>
+
