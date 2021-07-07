bce8ca13832146495a6bf96357c98e04dfd32f15
whyye <whyye@126.com>
2021-07-07 10:00:23 +0800

    布局登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,8 +3,34 @@
     <div class="login-wrap">
         <ul class="menu-tab">
           <li v-for="(item,index) in menuTab" :key=item.index  :class="{'current':item.current}" @click="toggleMenu(item)"> {{item.txt}}</li>
-          
         </ul>
+         <!-- 登录表单 -->
+         <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm"  class="login-form" >
+            <el-form-item  prop="email" class="item-from">
+              <label >邮箱</label>
+              <el-input type="password" v-model="ruleForm.email" autocomplete="off"></el-input>
+            </el-form-item>
+
+            <el-form-item  prop="pass" class="item-from">
+               <label >密码</label>
+              <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+            </el-form-item>
+
+            <el-form-item  prop="code" class="item-from">
+               <label>验证码</label>
+               <el-row :gutter="10">
+                  <el-col :span="15"> <el-input v-model.number="ruleForm.code"></el-input></el-col>
+                  <el-col :span="9"><el-button type="success"  class="block">获取验证码</el-button></el-col>
+                  
+               </el-row>
+             
+            </el-form-item>
+
+            <el-form-item  class="menu-login">
+              <el-button type="danger" @click="submitForm('ruleForm')" class="block">登录</el-button>
+            </el-form-item>
+        </el-form>
+
     </div>
   </div>
 </template>
@@ -14,11 +40,68 @@
 export default {
    name:"login",
    data() {
+    //  表单规则
+     var checkAge = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('年龄不能为空'));
+        }
+        setTimeout(() => {
+          if (!Number.isInteger(value)) {
+            callback(new Error('请输入数字值'));
+          } else {
+            if (value < 18) {
+              callback(new Error('必须年满18岁'));
+            } else {
+              callback();
+            }
+          }
+        }, 1000);
+      };
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.ruleForm.pass !== '') {
+            this.$refs.ruleForm.validateField('pass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.ruleForm.email) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        } 
+      };
      return {
        menuTab:[
         {txt:"登录",current:true },
         {txt:"注册",current:false }
-       ]
+       ],
+       //表单数据开始
+        ruleForm: {
+          email: '',
+          pass: '',
+          code: ''
+        },
+        rules: {
+          email: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+          pass: [
+            { validator: validatePass2, trigger: 'blur' }
+          ],
+          code: [
+            { validator: checkAge, trigger: 'blur' }
+          ]
+        }
+      
+       //表单数据结束
+       
+       
      }
    },
    methods:{
@@ -27,7 +110,23 @@ export default {
           el.current=false
        });
        data.current=true;
-     }
+     },
+
+    //  表单的方法
+         submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+     
+    
+    //  表单的方法
+    
    }
 }
 </script>
@@ -56,6 +155,23 @@ export default {
         }
       }
     }
+
+    .login-form{
+      margin-top: 29px;
+       .item-from {
+      margin-top: 13px;
+      color: #fff;
+    }
+    .menu-login{
+      margin-top: 30px;
+      .block {
+        display: block;
+        width: 100%;
+      }
+    }
+    }
+   
   }
+
 </style>>
 
