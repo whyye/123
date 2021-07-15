98085267b9b9a0dd13718bd830bbffcdd5cd16d7
whyye <whyye@126.com>
2021-07-15 16:35:37 +0800

    解决刷新页面 store数据跟新问题,用本地存储或cookies

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import Cookies from "js-cookie"
+
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    isCollapse:false
+    // 优先读取本地存储的值
+    isCollapse:JSON.parse(sessionStorage.getItem("isCollapse")) || false
+    
+    // isCollapse:  JSON.parse (Cookies.get('isCollapse')) || false
+   
   },
   getters :{
 
@@ -14,6 +20,11 @@ export default new Vuex.Store({
     SET_COLLAPSE (state,value) {
       
         state.isCollapse=!state.isCollapse
+        // 存储状态
+        sessionStorage.setItem("isCollapse", JSON.stringify(state.isCollapse))
+
+        // cookies
+        // Cookies.set("isCollapse", JSON.stringify(state.isCollapse))
     } 
   },
   actions: {},
